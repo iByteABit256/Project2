1af8b2b8568371e47cd527ae3b13ff8e808aaab4
iByteABit256 <paulsmith4561@gmail.com>
2021-12-07 19:01:43 +0200

    Google Tests

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/pauls/Documents/Project2
+# Build directory: /home/pauls/Documents/Project2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/home/pauls/Documents/Project2/build/my_test[1]_include.cmake")
+subdirs("_deps/googletest-build")
+subdirs("src")
+subdirs("lib")
---
+++ build/lib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pauls/Documents/Project2/lib
+# Build directory: /home/pauls/Documents/Project2/build/lib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
