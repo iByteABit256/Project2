6920d2bf3daf4a069e54a30484600c11134706c6
iByteABit256 <paulsmith4561@gmail.com>
2021-12-19 23:02:15 +0200

    new cli flags

--- src/cluster.cpp
+++ src/cluster.cpp
@@ -30,32 +30,47 @@ const int MIN_DIFF = 200;
 int main(int argc, char *argv[]){
 	int n;
 	int d;
-	string inputfile;
-	string outputfile;
-	string configurationfile;
-	string method;
-	bool complete = false;
+	string inputfile, outputfile, configurationfile;
+	string assignment_method, update_method;
+	bool complete = false, silhouettes_enabled = false;
 
 	int opt;
 
-	while ((opt = getopt(argc, argv, "i:c:o:Cm:")) != -1) {
+	while ((opt = getopt(argc, argv, "i:c:o:Csa:u:")) != -1) {
         switch (opt) {
+		// Input file
         case 'i':
             inputfile = optarg;
             break;
+		// Output file
         case 'o':
             outputfile = optarg;
             break;
+		// Config file
 		case 'c':
 			configurationfile = optarg;
 			break;
+		// Complete flag
 		case 'C':
 			complete = true;
 			break;
-		case 'm':
-			method = optarg;
-			if(method != "Classic" && method != "LSH" && method != "Hypercube"){
-				cerr << "Invalid method passed" << endl;
+		// Silhouette flag
+		case 's':
+			silhouettes_enabled = true;
+			break;
+		// Assignment Method
+		case 'a':
+			assignment_method = optarg;
+			if(assignment_method != "Classic" && assignment_method != "LSH" && assignment_method != "Hypercube"){
+				cerr << "Invalid assignment_method passed" << endl;
+				exit(EXIT_FAILURE);
+			}
+			break;
+		// Update assignment_method
+		case 'u':
+			update_method = optarg;
+			if(update_method != "Mean Frechet" && update_method != "Mean Vector"){
+				cerr << "Invalid assignment_method passed" << endl;
 				exit(EXIT_FAILURE);
 			}
 			break;
@@ -84,31 +99,33 @@ int main(int argc, char *argv[]){
 
 	// Initialization metric
 
-	vector<double> shortest_distances;
-	for(vector<Cluster *>::iterator c1 = clusters.begin(); c1 != clusters.end(); c1++){
-		double min_dist = numeric_limits<double>::max();
-		Point *centroid1 = (*c1)->centroid;
+	if(complete){
+		vector<double> shortest_distances;
+		for(vector<Cluster *>::iterator c1 = clusters.begin(); c1 != clusters.end(); c1++){
+			double min_dist = numeric_limits<double>::max();
+			Point *centroid1 = (*c1)->centroid;
 
-		for(vector<Cluster *>::iterator c2 = clusters.begin(); c2 != clusters.end(); c2++){
-			if(c1 == c2){
-				continue;
-			}	
+			for(vector<Cluster *>::iterator c2 = clusters.begin(); c2 != clusters.end(); c2++){
+				if(c1 == c2){
+					continue;
+				}	
 
-			Point *centroid2 = (*c2)->centroid;
-			double dist = centroid1->distance(*centroid2);
-			if(dist < min_dist){
-				min_dist = dist;
+				Point *centroid2 = (*c2)->centroid;
+				double dist = centroid1->distance(*centroid2);
+				if(dist < min_dist){
+					min_dist = dist;
+				}
 			}
+
+			shortest_distances.push_back(min_dist);
 		}
 
-		shortest_distances.push_back(min_dist);
+		for(vector<double>::iterator dist = shortest_distances.begin(); dist != shortest_distances.end(); dist++){
+			cerr << "Centroid " << dist-shortest_distances.begin() << " distance to closest centroid: " << *dist << endl;
+		}
+		double average = accumulate(shortest_distances.begin(), shortest_distances.end(), 0)/(double)shortest_distances.size();
+		cerr << "Average centroid distance to closest centroid: " << average << endl;
 	}
-
-	for(vector<double>::iterator dist = shortest_distances.begin(); dist != shortest_distances.end(); dist++){
-		cerr << "Centroid " << dist-shortest_distances.begin() << " distance to closest centroid: " << *dist << endl;
-	}
-	double average = accumulate(shortest_distances.begin(), shortest_distances.end(), 0)/(double)shortest_distances.size();
-	cerr << "Average centroid distance to closest centroid: " << average << endl;
 
     double dist;
 
@@ -116,7 +133,7 @@ int main(int argc, char *argv[]){
 
    	while(1){
 		// Assign points to current clusters
-	   	assignment(points,clusters,method,config);
+	   	assignment(points,clusters,assignment_method,config);
 
 		// Update all clusters
 	   	dist = 0;
@@ -139,19 +156,20 @@ int main(int argc, char *argv[]){
 	auto duration = chrono::duration_cast<chrono::milliseconds>(stop-start);
 	
 	vector<double> silhouette;
-
-	for(int i = 0; i < clusters.size(); i++){
-		double cs = clusterSilhouette(clusters[i], clusters);
-		silhouette.push_back(cs);
+	if(silhouettes_enabled){
+		for(int i = 0; i < clusters.size(); i++){
+			double cs = clusterSilhouette(clusters[i], clusters);
+			silhouette.push_back(cs);
+		}
+		
+		silhouette.push_back(totalSilhouette(clusters));
 	}
-	
-	silhouette.push_back(totalSilhouette(clusters));
 
 	ostringstream ss; // Intermediary output buffer
 
 	// Output
 
-	ss << "Algorithm: " << method << endl;	
+	ss << "Algorithm: " << assignment_method << endl;	
 	
 	for(int i = 0; i < clusters.size(); i++){
 		ss << "CLUSTER-" << i+1 << " {size: " << clusters[i]->points.size();
@@ -160,14 +178,16 @@ int main(int argc, char *argv[]){
 	
 	ss << "clustering_time: " << duration.count() << " ms" << endl;	
 	
-	ss << "Silhouette: [";
-	for(int i = 0; i < silhouette.size(); i++){
-		ss << silhouette[i];	
-		if(i+1 != silhouette.size()){
-			ss << ",";
+	if(silhouettes_enabled){
+		ss << "Silhouette: [";
+		for(int i = 0; i < silhouette.size(); i++){
+			ss << silhouette[i];	
+			if(i+1 != silhouette.size()){
+				ss << ",";
+			}
 		}
+		ss << "]" << endl;
 	}
-	ss << "]" << endl;
 	
 	if(complete){
 		for(int i = 0; i < clusters.size(); i++){
